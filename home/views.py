7cd7852c1d2b542a2b1d509279b91983bbed567f
Alabee <ianmutevu96@gmail.com>
2017-10-24 10:22:58 +0300

    Output of events in database

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, render_to_response
-
+from event_details.models import Event_details
 # Create your views here.
 def index(request):
-	return render_to_response('home/home.html', {'content': "This is the homepage"})
+	#events = Event_details.objects.all()
+	return render_to_response('home/home.html', {'content ': 'HEEEEEEY'})
