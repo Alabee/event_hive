a6aa2c1035b1d8c04aa15d0d677c7f7453ec14d9
Alabee <ianmutevu96@gmail.com>
2017-10-28 02:11:11 +0300

    Event categories update

--- review/urls.py
+++ review/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name = 'review'),
+	url(r'^art/', views.art, name = 'art'),
+	url(r'^partying/',views.partying, name = 'partying'),
+	url(r'^conferences/', views.conferences, name = 'conferences'),
+	url(r'^tech/', views.tech, name = 'tech'),
+
 ]
--- review/views.py
+++ review/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render, render_to_response
 def index(request):
 	return render_to_response('review/review.html')
 
+def art(request):
+	return render_to_response('review/review.html')
+
+def partying(request):
+	return render_to_response('review/review.html')
+
+def conferences(request):
+	return render_to_response('review/review.html')
+
+def tech(request):
+	return render_to_response('review/review.html')
+
