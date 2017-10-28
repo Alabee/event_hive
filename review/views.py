2f3aed49ad183caacf5637ce9b14a99be76e840e
Alabee <ianmutevu96@gmail.com>
2017-10-28 18:26:57 +0300

    Update of review templates

--- review/views.py
+++ review/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, render_to_response
+from event_details.models import Event_details
 
 # Create your views here.
 def index(request):
-	return render_to_response('review/review.html')
+	return render_to_response('review/review.html', {'content': Event_details.objects.all()})
 
 def art(request):
-	return render_to_response('review/review.html')
+	return render_to_response('review/art.html', {'content': Event_details.objects.all()})
 
 def partying(request):
-	return render_to_response('review/review.html')
+	return render_to_response('review/partying.html', {'content': Event_details.objects.all()})
 
 def conferences(request):
-	return render_to_response('review/review.html')
+	return render_to_response('review/conferences.html', {'content': Event_details.objects.all()})
 
 def tech(request):
-	return render_to_response('review/review.html')
-
+	return render_to_response('review/tech.html', {'content': Event_details.objects.all()})
